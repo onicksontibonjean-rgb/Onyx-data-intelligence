cb0fcb707105b7c3e4a645b8a5e9bf0ba30271e5
onicksontibonjean-rgb <onicksontibonjean@gmail.com>
2026-08-10 15:34:48 -0400

    Update 05-supabase-client.js

--- public/js/05-supabase-client.js
+++ public/js/05-supabase-client.js
@@ -8,7 +8,7 @@
  */
 
 window.ONYX_AUTH_CONFIG = Object.freeze({
-  supabaseUrl: "https://gnfswkaoktteqsprhmyr.supabase1.co",
+  supabaseUrl: "https://gnfswkaoktteqsprhmyr.supabase.co",
   supabasePublishableKey: "sb_publishable_D-tj4nD4d5T_nHzayOZdcQ_e9xO9upM",
   applicationName: "Onyx Data Intelligence"
 });
